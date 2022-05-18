a44e26f6265e69bf2e709ed8ba2cce85b539cf0e
Carmen Montiel <carmencita-2601@hotmail.com>
2022-05-18 01:03:33 -0500

    Terminé de modificar archivos aplicando el segundo punto del taller

--- js/hvd.js
+++ js/hvd.js
@@ -4,3 +4,22 @@ function cambiaFondo(x) {
     var body = document.getElementById("body");
     body.style.backgroundColor = x.value;
 }
+
+
+function acord(){
+    let elementosAcordeon = document.getElementsByClassName("acordeon");
+
+for(let i = 0; i < elementosAcordeon.length; i++){
+    elementosAcordeon[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+
+        let panel = this.nextElementSibling;
+
+        if(panel.style.display == "block"){
+            panel.style.display = "none";
+        }else{
+            panel.style.display = "block"
+        }
+    });
+}
+}
